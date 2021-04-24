789c8fa99f999b8c4fe3d428c116cac2c596b380
Vadym Shkurko <cn.klose5567@gmail.com>
2021-04-24 20:16:35 +0300

    learnt about data

---
+++ README.md
@@ -0,0 +1,2 @@
+# House prices project
+## Kaggle competition House Prices implemented by me myself
---
+++ settings/constants.py
@@ -0,0 +1,6 @@
+import os
+
+DATA_FOLDER = 'data'
+TRAIN_CSV = os.path.join(DATA_FOLDER, 'train.csv')
+VAL_CSV = os.path.join(DATA_FOLDER, 'val.csv')
+TEST_CSV = os.path.join(DATA_FOLDER, 'test.csv')
